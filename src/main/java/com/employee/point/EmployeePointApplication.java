b662155c620d12b854044802246b18ef9c5697a4
Thomas <ThomasAdhi.Nugroho@mitrais.com>
2018-12-13 08:21:01 +0700

    First Commit

---
+++ src/main/java/com/employee/point/EmployeePointApplication.java
@@ -0,0 +1,14 @@
+package com.employee.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePointApplication.class, args);
+	}
+
+}
+
